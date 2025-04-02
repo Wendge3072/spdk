27100192baa8901da5f72de4fe5598383ee2c66c
Wendge3072 <wendge@outlook.com>
2025-04-02 17:06:54 +0800

    ftl

--- module/bdev/ftl/bdev_ftl.h
+++ module/bdev/ftl/bdev_ftl.h
@@ -32,7 +32,7 @@ int bdev_ftl_create_bdev(const struct spdk_ftl_conf *conf, ftl_bdev_init_fn cb, 
 void bdev_ftl_delete_bdev(const char *name, bool fast_shutdown, spdk_bdev_unregister_cb cb_fn,
 			  void *cb_arg);
 void bdev_ftl_set_bggc_comp_threshold(const char *name, uint32_t bggc_comp_threshold,
-				      ftl_bdev_thread_fn cb_fn, void *cb_arg);
+			spdk_ftl_fn cb_fn, void *cb_arg);
 int bdev_ftl_defer_init(const struct spdk_ftl_conf *conf);
 void bdev_ftl_unmap(const char *name, uint64_t lba, uint64_t num_blocks, spdk_ftl_fn cb_fn,
 		    void *cb_arg);
