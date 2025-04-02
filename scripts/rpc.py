5edfab86ece661005b071470e23bd9d908fdba32
Wendge3072 <wendge@outlook.com>
2025-04-02 17:41:17 +0800

    ftl

--- scripts/rpc.py
+++ scripts/rpc.py
@@ -2034,10 +2034,8 @@ Format: 'user:u1 secret:s1 muser:mu1 msecret:ms1,user:u2 secret:s2 muser:mu2 mse
 
     def bdev_ftl_set_bggc_comp_threshold(args):
         print("calling bdev_ftl_set_bggc_comp_threshold 1")
-        print_dict(rpc.bdev.bdev_ftl_set_bggc_comp_threshold(args.client,
-                                                             name=args.name,
-                                                             threshold=args.threshold))
-    
+        print_dict(rpc.bdev.bdev_ftl_set_bggc_comp_threshold(args.client, name=args.name, threshold=args.threshold))
+
     p = subparsers.add_parser('bdev_ftl_set_bggc_comp_threshold', help='Set FTL bdev background garbage collection compaction threshold')
     p.add_argument('-b', '--name', help="Name of the bdev", required=True)
     p.add_argument('-t', '--threshold', help='Background garbage collection compaction threshold (MiB/s)', required=True, type=int)
